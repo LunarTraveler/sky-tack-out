85d2af8b015bf2e9de11371fe52fe8fe9c5db2c4
LunarTraveler <2961478685@qq.com>
2024-10-07 21:47:04 +0800

    使用redisTemplate工具来缓存菜品

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -11,9 +11,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("admin/dish")
@@ -24,6 +26,17 @@ public class DishController {
 
     private final DishService dishService;
 
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 用于统一清理redis的缓存数据
+     * @param pattern
+     */
+    private void cacheClean(String pattern) {
+        Set<String> keys = redisTemplate.keys(pattern);
+        redisTemplate.delete(keys);
+    }
+
     /**
      * 新增菜品和对应的口味
      * @param dishDTO
@@ -33,6 +46,11 @@ public class DishController {
     @PostMapping()
     public Result saveWithFlavor(@RequestBody DishDTO dishDTO) {
         dishService.saveWithFlavor(dishDTO);
+
+        // 清理缓存数据，因为这个分类新增了一个菜品，前台展示的时候是没有这条数据的，所以要全部清理(但是只是涉及一个分类)
+        String key = "dish_" + dishDTO.getCategoryId();
+        cacheClean(key);
+
         return Result.success();
     }
 
@@ -57,6 +75,8 @@ public class DishController {
     @ApiOperation("批量删除菜品")
     public Result delete(@RequestParam List<Long> ids) {
         dishService.deleteBatch(ids);
+        // 可能会涉及多个分类，为了不查询数据库，所以全部删除
+        cacheClean("dish_*");
         return Result.success();
     }
 
@@ -81,6 +101,8 @@ public class DishController {
     @ApiOperation("修改菜品信息")
     public Result updateDishWithFlavor(@RequestBody DishDTO dishDTO) {
         dishService.updateDishWithFlavor(dishDTO);
+        // 可能会涉及一个或两个分类，为了不查询数据库，所以全部删除
+        cacheClean("dish_*");
         return Result.success();
     }
 
@@ -93,6 +115,8 @@ public class DishController {
     @ApiOperation("启用禁用分类")
     public Result<String> startOrStop(@PathVariable("status") Integer status, Long id){
         dishService.startOrStop(status,id);
+        // 这里好像只涉及一个分类吧(但是这个分类要查询数据库...)
+        cacheClean("dish_*");
         return Result.success();
     }
 
--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,8 @@ public class DishController {
 
     private final DishService dishService;
 
+    private final RedisTemplate<String, Object> redisTemplate;
+
     /**
      * 根据分类id查询菜品
      * @return
@@ -34,13 +37,21 @@ public class DishController {
     public Result<List<DishVO>> list(Long categoryId) {
         log.info("根据分类id查询菜品{}", categoryId);
 
+        // 热点数据加上缓存
+        String key = "dish_" + categoryId;
+        List<DishVO> dishList = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        if (dishList != null && dishList.size() > 0) {
+            return Result.success(dishList);
+        }
+
         // 把分类的条件id和在销售中这两个条件封装到Dish中(这里要注意了，当时我封装的是map)
         Map<String, Object> params = new HashMap<>();
         params.put("categoryId", categoryId);
         params.put("status", StatusConstant.ENABLE);
 
-        List<DishVO> list = dishService.listWithFlavor(params);
-        return Result.success(list);
+        dishList = dishService.listWithFlavor(params);
+        redisTemplate.opsForValue().set(key, dishList);
+        return Result.success(dishList);
     }
 
 
